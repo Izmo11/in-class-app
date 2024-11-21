ae07bbe93827388d13335ee39adf1c0b3e275aa0
Izmo11 <185049677+Izmo11@users.noreply.github.com>
2024-11-21 15:56:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ import multiplication
 print("Using addition")
 x = add_two_numbers(1, 2)
 print("1 + 2 = " + x)
+
+print("Using subtraction:")
+x = subtraction.subtract_two_numbers(10, 5)
+print("10 - 5 = " + str(x))
+
+print("Using multiplication:")
+y = multiplication.multiply_two_numbers(4, 3)
+print("4 * 3 = " + str(y))
+
+print("Using square root:")
+z = squareroot.square_root(16)
+print("√16 = " + str(z))
